c6974e8b9f6cc47a801de255f2419bb85d1e1306
Julian Akkaya <jak@wegewerk.com>
2017-07-13 12:02:49 +0200

    add new function "printNewLocation"

--- actions.py
+++ actions.py
@@ -1,20 +1,27 @@
 def go (nextPlaceId, player, welt):
     safeCurrentPlace(player, welt)
     changePlace(nextPlaceId, player, welt)
+    printNewLocation(player)
 
 def safeCurrentPlace(player, welt):
-    currentPlace = player.currentPlace.currentId
-    if currentPlace == 1:
-        welt.baeker = player.currentPlace
-    if currentPlace == 2:
+    if player.currentPlace.currentId == 1:
         welt.dorfplatz = player.currentPlace
+    if player.currentPlace.currentId == 2:
+        welt.baeker = player.currentPlace
+
+def printNewLocation(player):
+    print player.currentPlace.description
+    print "\n"
+    print "north: " + player.currentPlace.north[0]
+    print "east: " + player.currentPlace.east[0]
+    print "south: " + player.currentPlace.south[0]
+    print "west: " + player.currentPlace.west[0]
+    print "\n"
 
 def changePlace(nextPlaceId, player, welt):
     if nextPlaceId == 0:
         print "Das ist nicht moeglich."
     elif nextPlaceId == 1:
         player.currentPlace = welt.dorfplatz
-        print player.currentPlace.description
     elif nextPlaceId == 2:
         player.currentPlace = welt.baeker
-        print player.currentPlace.description
